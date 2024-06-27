61fe7580619261d4bbde0f4c6a1e542a3bd3314d
Carlos Peña <cgabriel2303@gmail.com>
2024-06-27 14:54:18 -0400

    add imgs

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home,loginV,registerV
 
 urlpatterns = [
     path('', home, name='home'),
+    path('login', loginV, name='login'),
+    path('register', registerV, name='register'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,4 +3,10 @@ from .models import Product
 # Create your views here.
 def home(req):
     products = Product.objects.all()
-    return render(req, 'home/home.html', {'products': products})
+    return render(req, 'home/index.html', {'products': products})
+
+def loginV(req):
+    return render(req,'login/index.html') 
+
+def registerV(req):
+    return render(req,'register/index.html')
