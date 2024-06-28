e4a1ea850967524defb4f55f4aa8cf34a4d4c7d8
Carlos Peña <cgabriel2303@gmail.com>
2024-06-28 01:54:31 -0400

    add login, logout

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home,loginV,registerV,prodDetail
+from .views import home,loginV,registerV,prodDetail,logoutV
 
 urlpatterns = [
     path('', home, name='home'),
     path('login', loginV, name='login'),
+    path('logout', logoutV, name='logout'),
     path('register', registerV, name='register'),
     path('product/<int:id>', prodDetail, name='prodDetail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Product
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 def home(req):
     products = Product.objects.all()
     return render(req, 'home/index.html', {'products': products})
 
 def loginV(req):
+    if req.method=='POST':
+        username = req.POST['username']
+        password = req.POST['pass']
+
+        user=authenticate(req,username=username,password=password)
+
+        if user is not None:
+            login(req,user)
+            return redirect('home')
+        else:
+            messages={
+                'error':'Credenciales invalidas'
+            }
+            return render(req,'login/index.html',messages)
+
     return render(req,'login/index.html') 
+
+def logoutV(req):
+    logout(req)
+    return redirect('login')
 
 def registerV(req):
     return render(req,'register/index.html')
