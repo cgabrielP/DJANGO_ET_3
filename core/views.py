33ffeed278562ff4106c7821b8d9f434708bd197
Carlos Peña <cgabriel2303@gmail.com>
2024-06-29 14:31:14 -0400

    add register view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .models import Product
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+
 # Create your views here.
 def home(req):
     products = Product.objects.all()
@@ -29,6 +31,21 @@ def logoutV(req):
     return redirect('login')
 
 def registerV(req):
+    if req.method=='POST':
+        first_name=req.POST['first_name']
+        last_name=req.POST['last_name']
+        email = req.POST['email']
+        username=email.split('@')[0] 
+        password = req.POST['pass']
+        user = User.objects.create_user(username=username,password=password,email=email,first_name=first_name,last_name=last_name)
+        if user is not None:
+            return redirect('login')
+        else:
+            messages={
+                'error':'Datos invalidos'
+            }
+            return render(req,'register/index.html',messages)
+        
     return render(req,'register/index.html')
 
 def prodDetail(req,id):
